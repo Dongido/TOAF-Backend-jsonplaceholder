bac993609ed8fd0e4e1e5157d69168ab5dbade60
Dongido <chima.sima@gmail.com>
2022-08-05 17:01:26 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { GetPhotos } from './Components/GetPhotos'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( 
+      <div className="container">
+        <div class="container py-2">
+          <div class="p-5 mb-4 bg-light rounded-3">
+            <div class="container-fluid py-3">
+              <h1 class="display-5 fw-bold">Photo Gallery</h1>
+              <p class="col-md-8 fs-4"> Welcome to photo gallery. 
+              Data source from <b>json placeholder </b> </p> 
+            </div>
+          </div>
+        </div>
+
+        <GetPhotos />
+      </div>
+      
+    );
 }
 
 export default App;
---
+++ src/Components/GetPhotos.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export function GetPhotos() {
+
+    const [photos, setPhotos] = useState([]);
+
+    const getPhotos = async () => {
+    try {
+        const response = await axios.get("https://jsonplaceholder.typicode.com/photos")
+        setPhotos(response.data);
+        console.log(response);
+
+      } catch (err) {
+        console.error(err.message);
+      }
+    };
+
+    useEffect(() => {
+        getPhotos();
+
+        const interval = setInterval(()=>{
+          getPhotos()
+         },60000)
+       
+        return()=>clearInterval(interval)
+    }, [])
+
+    return ( 
+        <div className="row">
+            {photos.map(photo=>(
+                <div className="col-md-3 py-2">
+                    <div key={photo.id} className="card">
+                      <img src={photo.thumbnailUrl} className="card-img-top" alt={photo.title} />
+                      <div className="card-body">
+                        <h5 className="card-title">{photo.title}</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href={photo.url} className="btn btn-primary">View</a>
+                      </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+
+}
+
+export default GetPhotos;
